6a2787e65bb74936c434e237b1cdd3596616b5f5
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-05-19 07:41:12 +0530

    update: initiate hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Heading>Hello world</Heading>
+    </Box>
   );
 }
 
