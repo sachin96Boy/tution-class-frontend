fcbe9a88be86cab6579b36dd7a6143d1004644d0
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-07-20 15:29:40 +0530

    init: firebase init n project

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,24 @@ import HomePage from "./pages/HomePage";
 import MyAccount from "./pages/MyAccount";
 import MyCourses from "./pages/MyCourses";
 import Signin from "./pages/Signin";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  console.log(analytics);
   return (
     <Router>
       <Box
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Center, Flex, Image } from "@chakra-ui/react";
+import Sipsalogo from "../assets/signin/Sipsa_logo.png";
+import React from "react";
 
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <Flex>
+      <Center>
+        <Image src={Sipsalogo} alt="Sipsa Institute" />
+      </Center>
+    </Flex>
+  );
 }
 
-export default Signup
+export default Signup;
