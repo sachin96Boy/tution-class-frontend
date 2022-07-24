73df838d6477ad6d1ac9e372bca21b8e0aba9e2f
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-07-24 17:11:15 +0530

    update: esson accordian

---
+++ src/components/course/LessonlementCard.tsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+
+interface LessonEementCardProps {
+  grade: string;
+  lessonName: string;
+  date: string;
+  viewResource: string;
+  attendNow: string;
+  imgSrc: string;
+}
+
+function LessonlementCard({
+  grade,
+  lessonName,
+  date,
+  viewResource,
+  attendNow,
+  imgSrc,
+}: LessonEementCardProps) {
+  return (
+    <Box my={2} p={4} maxH={"230px"} bg="#FFFFFF" rounded={"16px"}>
+      <Flex flexDir={"column"} align="center">
+        <Image
+          objectFit={"cover"}
+          rounded={"12px"}
+          src={imgSrc}
+          alt={lessonName}
+          w={"23p8x"}
+          h={"109px"}
+        />
+        <Flex align={"center"} justify={"center"}>
+          <Box gap={2}>
+            <Button
+              bgGradient={
+                "linear-gradient(94.16deg, #F4BB4E 2.33%, #A06D3A 100%)"
+              }
+              color={"white"}
+              rounded={"10px"}
+              colorScheme={"yellow"}
+              px={3}
+              h={"20px"}
+            >
+              <Text fontFamily={"body"} fontWeight="400" fontSize={"12px"}>
+                {grade}
+              </Text>
+            </Button>
+            <Heading
+              as={"h3"}
+              color="#000000"
+              fontFamily={"body"}
+              fontSize={"24px"}
+              fontWeight="600"
+            >
+              {lessonName}
+            </Heading>
+            <Text
+              color={"#7E7E7E"}
+              fontFamily={"body"}
+              fontSize="12px"
+              fontWeight={"400"}
+            >
+              {date}
+            </Text>
+            <Link onClick={() => window.open(viewResource)}>View Resource</Link>
+          </Box>
+          <Flex align={"center"} justify={"center"}>
+            <Button
+              w={"75px"}
+              h={"52px"}
+              color={"white"}
+              rounded={"5px"}
+              colorScheme={"blackAlpha"}
+              px={3}
+              onClick={() => window.open(attendNow)}
+            >
+              <Text fontFamily={"body"} fontWeight="600" fontSize={"12px"}>
+                Attend Now
+              </Text>
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
+
+export default LessonlementCard;
--- src/components/course/LessonlistAccordian.tsx
+++ src/components/course/LessonlistAccordian.tsx
@@ -1,9 +1,145 @@
-import React from 'react'
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Heading,
+} from "@chakra-ui/react";
+import React from "react";
+import LessonlementCard from "./LessonlementCard";
+
+export interface LessonContent {
+  week: string;
+  date: string;
+  lessonName: string;
+  imgSrc: string;
+  lessonContent: string;
+  viewResource: string;
+  AttendNow: string;
+}
+
+interface LessonDetailsListProps {
+  subject: string;
+  grade: string;
+  year: string;
+  month: string;
+  content: Array<LessonContent>;
+}
+
+const lessonList: Array<LessonDetailsListProps> = [
+  {
+    subject: "Biology",
+    grade: "10",
+    year: "2020",
+    month: "Mar",
+    content: [
+      {
+        week: "Week 1",
+        date: "03/03/2020",
+        lessonName: "Lesson 1",
+        imgSrc: "https://picsum.photos/200",
+        lessonContent: "This is the content of lesson 1",
+        viewResource: "View Resource",
+        AttendNow: "Attand Now",
+      },
+      {
+        week: "Week 2",
+        date: "03/03/2020",
+        lessonName: "Lesson 2",
+        imgSrc: "https://picsum.photos/200",
+        lessonContent: "This is the content of lesson 2",
+        viewResource: "View Resource",
+        AttendNow: "Attand Now",
+      },
+      {
+        week: "Week 3",
+        date: "03/03/2020",
+        lessonName: "Lesson 3",
+        imgSrc: "https://picsum.photos/200",
+        lessonContent: "This is the content of lesson 3",
+        viewResource: "View Resource",
+        AttendNow: "Attand Now",
+      },
+    ],
+  },
+  {
+    subject: "Math",
+    grade: "10",
+    year: "2020",
+    month: "Mar",
+    content: [
+      {
+        week: "Week 1",
+        date: "03/03/2020",
+        lessonName: "Lesson 1",
+        imgSrc: "https://picsum.photos/200",
+        lessonContent: "This is the content of lesson 1",
+        viewResource: "View Resource",
+        AttendNow: "Attand Now",
+      },
+      {
+        week: "Week 2",
+        date: "03/03/2020",
+        lessonName: "Lesson 2",
+        imgSrc: "https://picsum.photos/200",
+        lessonContent: "This is the content of lesson 2",
+        viewResource: "View Resource",
+        AttendNow: "Attand Now",
+      },
+      {
+        week: "Week 3",
+        date: "03/03/2020",
+        lessonName: "Lesson 3",
+        imgSrc: "https://picsum.photos/200",
+        lessonContent: "This is the content of lesson 3",
+        viewResource: "View Resource",
+        AttendNow: "Attand Now",
+      },
+    ],
+  },
+];
 
 function LessonlistAccordian() {
   return (
-    <div>LessonlistAccordian</div>
-  )
+    <Box>
+      {lessonList.map((lessonContent, index) => (
+        <Accordion key={index} defaultIndex={[0]} allowToggle>
+          <AccordionItem>
+            <Heading>
+              <AccordionButton>
+                {lessonContent.month} {lessonContent.year}
+                <AccordionIcon />
+              </AccordionButton>
+            </Heading>
+
+            <AccordionPanel p={2}>
+              <Flex
+                bg="#E6F1FF"
+                align={"center"}
+                justify="space-evenly"
+                gap={1}
+              >
+                {lessonContent.content.map((lesson, index) => (
+                  <LessonlementCard
+                    key={index}
+                    grade={lessonContent.grade}
+                    date={lesson.date}
+                    imgSrc={lesson.imgSrc}
+                    lessonName={lesson.lessonName}
+                    attendNow={lesson.AttendNow}
+                    viewResource={lesson.viewResource}
+                  />
+                ))}
+              </Flex>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      ))}
+    </Box>
+  );
 }
 
-export default LessonlistAccordian
+export default LessonlistAccordian;
