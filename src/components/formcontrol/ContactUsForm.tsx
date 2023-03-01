56939be0a664cd56202a4b83c1dbc0ff5e834367
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-03-01 19:23:55 +0530

    chor: inprogress on contact

--- src/components/formcontrol/ContactUsForm.tsx
+++ src/components/formcontrol/ContactUsForm.tsx
@@ -3,6 +3,7 @@ import { Form, Formik } from "formik";
 import React from "react";
 
 import * as Yup from "yup";
+import InputComponent from "./InputComponent";
 
 type ContactFormProps = {
   fullName: string;
@@ -37,15 +38,45 @@ function ContactUsForm() {
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
-    >{
-        (formik)=>(
-            <Form>
-                <VStack spacing={4}>
-                    
-                </VStack>
-            </Form>
-        )
-    }</Formik>
+    >
+      {(formik) => (
+        <Form>
+          <VStack spacing={4}>
+            <InputComponent
+              htmlFor="name"
+              placeHolder="Full Name"
+              labelText="Full Name"
+              InputType={"text"}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              InputValue={formik.values.fullName}
+              formikError="fullName"
+            />
+            <InputComponent
+              htmlFor="email"
+              placeHolder="Email address"
+              labelText="Email Address"
+              InputType={"email"}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              InputValue={formik.values.email}
+              formikError="email"
+            />
+            <InputComponent
+              htmlFor="grade"
+              placeHolder="User Grade"
+              labelText="User Grade"
+              InputType={"text"}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              InputValue={formik.values.grade}
+              formikError="grade"
+            />
+            
+          </VStack>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,10 +1,11 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import ContactUsForm from "../components/formcontrol/ContactUsForm";
 import Logo from "../components/Logo";
 
 function Support() {
   return (
-    <Flex>
+    <Flex alignItems={"center"} justify={"space-between"} mx={"10"}>
       {/* contact Us Section */}
       <Flex
         flexDirection={"column"}
@@ -15,7 +16,7 @@ function Support() {
         <Text fontFamily={"body"} fontSize={"3xl"} fontWeight={"bold"}>
           Write Us Your Questions
         </Text>
-        
+        <ContactUsForm />
       </Flex>
       {/* Sipsa Logo */}
       <Logo boxSize={"52"} linkPath={"/"} fitType={"Cover"} />
