6feb6a2756d7dc122064aa023d5a734e2e0fdbd5
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-05-07 12:32:22 +0530

    chor: state updte on loading and error

--- src/pages/admin/routes/AdminRoutes.tsx
+++ src/pages/admin/routes/AdminRoutes.tsx
@@ -21,38 +21,65 @@ import AdminSetting from "../pages/AdminSetting";
 import AdminAdvStudent from "../pages/student/AdminAdvStudent";
 import AdminnicStudent from "../pages/student/AdminnicStudent";
 import AdminRequests from "../pages/AdminRequests";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { Center, Flex, ProgressCircle } from "@chakra-ui/react";
 
 function AdminRoutes() {
+  const { loading, error, errorMsg } = useSelector(
+    (state: RootState) => state.config
+  );
+
   return (
-    <Routes>
-      <Route element={<AdminRoute />}>
-        <Route element={<AdminLayout />}>
-          <Route path="/teachers" element={<AdminTeachers />} />
-          <Route path="/time-table/data" element={<AdminTimeTableData />} />
-          <Route path="/time-table" element={<AdminTimeTables />} />
-          <Route path="/dashboard" element={<AdminDashboard />} />
-          <Route path="/attandance" element={<AdminAttandance />} />
-          <Route path="/advertisments" element={<AdminAdvertiments />} />
-          <Route path="/reports" element={<AdminReports />} />
-          <Route path="/users" element={<AdminUsers />} />
-          <Route path="/students" element={<Students />} />
-          <Route path="/requests" element={<AdminRequests />} />
-          <Route path="/students/advance/:id" element={<AdminAdvStudent />} />
-          <Route path="/students/nic/:id" element={<AdminnicStudent />} />
-          <Route path="/assignments/data" element={<AssignmentData />} />
-          <Route path="/assignments" element={<Assignments />} />
-          <Route path="/courses" element={<AdminCourses />} />
-          <Route path="/courses/data" element={<AdminCoursesData />} />
-          <Route path="/common" element={<AdminCommonSection />} />
-          <Route path="/accounting" element={<AdminAccounting />} />
-          <Route path="/settings" element={<AdminSetting />} />
-          <Route
-            path="*"
-            element={<Navigate to={"/admin/dashboard"} replace />}
-          />
-        </Route>
-      </Route>
-    </Routes>
+    <>
+      {loading ? (
+        <Flex minH={"100vh"} align={"center"} justify={"center"}>
+          <Center>
+            <ProgressCircle.Root value={null}>
+              <ProgressCircle.Circle>
+                <ProgressCircle.Track />
+                <ProgressCircle.Range stroke="orange" />
+              </ProgressCircle.Circle>
+            </ProgressCircle.Root>
+          </Center>
+        </Flex>
+      ) : error ? (
+        <Center>{errorMsg.toString()}</Center>
+      ) : (
+        <Routes>
+          <Route element={<AdminRoute />}>
+            <Route element={<AdminLayout />}>
+              <Route path="/teachers" element={<AdminTeachers />} />
+              <Route path="/time-table/data" element={<AdminTimeTableData />} />
+              <Route path="/time-table" element={<AdminTimeTables />} />
+              <Route path="/dashboard" element={<AdminDashboard />} />
+              <Route path="/attandance" element={<AdminAttandance />} />
+              <Route path="/advertisments" element={<AdminAdvertiments />} />
+              <Route path="/reports" element={<AdminReports />} />
+              <Route path="/users" element={<AdminUsers />} />
+              <Route path="/students" element={<Students />} />
+              <Route path="/requests" element={<AdminRequests />} />
+              <Route
+                path="/students/advance/:id"
+                element={<AdminAdvStudent />}
+              />
+              <Route path="/students/nic/:id" element={<AdminnicStudent />} />
+              <Route path="/assignments/data" element={<AssignmentData />} />
+              <Route path="/assignments" element={<Assignments />} />
+              <Route path="/courses" element={<AdminCourses />} />
+              <Route path="/courses/data" element={<AdminCoursesData />} />
+              <Route path="/common" element={<AdminCommonSection />} />
+              <Route path="/accounting" element={<AdminAccounting />} />
+              <Route path="/settings" element={<AdminSetting />} />
+              <Route
+                path="*"
+                element={<Navigate to={"/admin/dashboard"} replace />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 }
 
